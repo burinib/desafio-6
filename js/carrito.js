46d109f090e3b5cfca745bcb1496f059578ea70e
Braian Burini <braianb55@hotmail.com>
2022-07-13 13:17:33 -0300

    agregando datajs

--- js/carrito.js
+++ js/carrito.js
@@ -1,420 +1,64 @@
-const tienda = [
-    {
-      nombre: "abrigo1",
-      precio: 3490,
-      src: "/abrigo/buzo1.jpg",
-      id: "0",
-    },
-    {
-      nombre: "abrigo2",
-      precio: 3590,
-      src: "/abrigo/buzo2.jpg",
-      id: "1",     
-    },
-    {
-      nombre: "abrigo3",
-      precio: 3490,
-      src: "/abrigo/buzo3.jpg",
-      id: "2"
-    },
-    {
-      nombre: "abrigo4",
-      precio: 2590,
-      src: "/abrigo/buzo4.jpg",
-      id: "3"
-    },
-    {
-      nombre: "abrigo5",
-      precio: 3490,
-      src: "/abrigo/buzo5.jpg",
-      id: "4"
-    },
-    {
-      nombre: "abrigo6",
-      precio: 9990,
-      src: "/abrigo/parca1.jpg",
-      id: "5"
-    },
-    {
-      nombre: "abrigo7",
-      precio: 10490,
-      src: "/abrigo/parca2.jpg",
-      id: "6"
-    },
-    {
-      nombre: "abrigo8",
-      precio: 12390,
-      src: "/abrigo/parca3.jpg",
-      id: "7"
-    },
-    {
-      nombre: "abrigo9",
-      precio: 8990,
-      src: "/abrigo/parca4.jpg",
-      id: "8"
-    },
-    {
-      nombre: "abrigo10",
-      precio: 11990,
-      src: "/abrigo/parca5.jpg",
-      id: "9"
-    },
-    {
-      nombre: "abrigo11",
-      precio: 9990,
-      src: "/abrigo/abrigo6.webp",
-      id: "10"
-    },
-    {
-      nombre: "abrigo12",
-      precio: 10490,
-      src: "/abrigo/abrigo7.webp",
-      id: "11"
-    },
-    {
-      nombre: "abrigo13",
-      precio: 12390,
-      src: "/abrigo/abrigo8.webp",
-      id: "12"
-    },
-    {
-      nombre: "abrigo14",
-      precio: 8990,
-      src: "/abrigo/abrigo9.jpg",
-      id: "13"
-    },
-    {
-      nombre: "abrigo15",
-      precio: 11990,
-      src: "/abrigo/abrigo10.jpg",
-      id: "14"
-    },
-    {
-      nombre: "abrigo16",
-      precio: 9990,
-      src: "/abrigo/abrigo1.jpg",
-      id: "15"
-    },
-    {
-      nombre: "abrigo17",
-      precio: 10490,
-      src: "/abrigo/abrigo2.jpg",
-      id: "16"
-    },
-    {
-      nombre: "abrigo18",
-      precio: 12390,
-      src: "/abrigo/abrigo3.jpg",
-      id: "17"
-    },
-    {
-      nombre: "abrigo19",
-      precio: 8990,
-      src: "/abrigo/abrigo4.jpg",
-      id: "18"
-    },
-    {
-      nombre: "abrigo20",
-      precio: 11990,
-      src: "/abrigo/abrigo5.jpg",
-      id: "19"
-    },
-    {
-      nombre: "camisa1",
-      precio: 3590,
-      src: "/camisa/camisa1.jpg",
-      id: "20",
-    },
-    {
-      nombre: "camisa2",
-      precio: 3590,
-      src: "/camisa/camisa2.jpg",
-      id: "21",
-    },
-    {
-      nombre: "camisa3",
-      precio: 2990,
-      src: "/camisa/camisa3.jpg",
-      id: "22",
-    },
-    {
-      nombre: "camisa4",
-      precio: 2990,
-      src: "/camisa/camisa4.jpg",
-      id: "23",
-    },
-    {
-      nombre: "camisa5",
-      precio: 2990,
-      src: "/camisa/camisa5.jpg",
-      id: "24",
-    },
-    {
-      nombre: "camisa6",
-      precio: 1990,
-      src: "/camisa/camisa6.jpg",
-      id: "25",
-    },
-    {
-      nombre: "camisa7",
-      precio: 2350,
-      src: "/camisa/camisa7.jpg",
-      id: "26",
-    },
-    {
-      nombre: "camisa8",
-      precio: 2990,
-      src: "/camisa/camisa8.jpg",
-      id: "27",
-    },
-    {
-      nombre: "camisa9",
-      precio: 2790,
-      src: "/camisa/camisa9.jpg",
-      id: "28",
-    },
-    {
-      nombre: "camisa10",
-      precio: 2390,
-      src: "/camisa/camisa10.jpg",
-      id: "29",
-    },
-    {
-      nombre: "camisa11",
-      precio: 1990,
-      src: "/camisa/camisa12.jpg",
-      id: "30",
-    },
-    {
-      nombre: "camisa12",
-      precio: 2350,
-      src: "/camisa/camisa13.jpg",
-      id: "31",
-    },
-    {
-      nombre: "camisa13",
-      precio: 2990,
-      src: "/camisa/camisa14.jpg",
-      id: "32",
-    },
-    {
-      nombre: "camisa14",
-      precio: 2790,
-      src: "/camisa/camisa15.webp",
-      id: "33",
-    },
-    {
-      nombre: "camisa15",
-      precio: 2390,
-      src: "/camisa/camisa16.webp",
-      id: "34",
-    },
-    {
-      nombre: "camisa16",
-      precio: 1990,
-      src: "/camisa/camisa17.jpg",
-      id: "35",
-    },
-    {
-      nombre: "camisa17",
-      precio: 2350,
-      src: "/camisa/camisa18.jpg",
-      id: "36",
-    },
-    {
-      nombre: "camisa18",
-      precio: 2990,
-      src: "/camisa/camisa19.jpg",
-      id: "37",
-    },
-    {
-      nombre: "camisa19",
-      precio: 2790,
-      src: "/camisa/camisa20.jpg",
-      id: "38",
-    },
-    {
-      nombre: "camisa20",
-      precio: 2390,
-      src: "/camisa/camisa11.jpg",
-      id: "39",
-    },
-    {
-      nombre: "pantalon1",
-      precio: 7590,
-      src:"/pantalon/pantalon1.jpg", 
-      id: "40",
-    },
-    {
-      nombre: "pantalon2",
-      precio: 6990,
-      src:"/pantalon/pantalon2.webp",
-      id: "41"
-    },
-    {
-      nombre: "pantalon3",
-      precio: 5990,
-      src:"/pantalon/pantalon3.jpg", 
-      id: "42",
-    },
-    {
-      nombre: "pantalon4",
-      precio: 5890,
-      src:"/pantalon/pantalon4.jpg", 
-      id: "43",
-    },
-    {
-      nombre: "pantalon5",
-      precio: 5900,
-      src:"/pantalon/pantalon5.jpg", 
-      id: "44",
-    },
-    {
-      nombre: "pantalon6",
-      precio: 8940,
-      src:"/pantalon/pantalon6.jpg", 
-      id: "45",
-    },
-    {
-      nombre: "pantalon7",
-      precio: 7590,
-      src:"/pantalon/pantalon7.webp",
-      id: "46",
-    },
-    {
-      nombre: "pantalon8",
-      precio: 6990,
-      src:"/pantalon/pantalon8.jpg", 
-      id: "47",
-    },
-    {
-      nombre: "pantalon9",
-      precio: 5990,
-      src:"/pantalon/pantalon9.jpg", 
-      id: "48",
-    },
-    {
-      nombre: "pantalon10",
-      precio: 6990,
-      src:"/pantalon/pantalon10.jpg",
-      id: "49",
-    },
-    {
-      nombre: "pantalon11",
-      precio: 8940,
-      src:"/pantalon/pantalon11.jpg",
-      id: "50",
-    },
-    {
-      nombre: "pantalon12",
-      precio: 7590,
-      src:"/pantalon/pantalon12.jpg",
-      id: "51",
-    },
-    {
-      nombre: "pantalon13",
-      precio: 6990,
-      src:"/pantalon/pantalon13.jpg",
-      id: "52",
-    },
-    {
-      nombre: "pantalon14",
-      precio: 5990,
-      src:"/pantalon/pantalon14.jpg",
-      id: "53",
-    },
-    {
-      nombre: "pantalon15",
-      precio: 6990,
-      src:"/pantalon/pantalon15.jpg",
-      id: "54",
-    },
-    {
-      nombre: "pantalon16",
-      precio: 8490,
-      src:"/pantalon/pantalon16.jpg",
-      id: "55",
-    },
-    {
-      nombre: "pantalon17",
-      precio: 7590,
-      src:"/pantalon/pantalon17.jpg",
-      id: "56",
-    },
-    {
-      nombre: "pantalon18",
-      precio: 6990,
-      src:"/pantalon/pantalon18.jpg",
-      id: "57",
-    },
-    {
-      nombre: "pantalon19",
-      precio: 5990,
-      src:"/pantalon/pantalon19.jpg",
-      id: "58",
-    },
-    {
-      nombre: "pantalon20",
-      precio: 16990,
-      src:"/pantalon/pantalon20.jpg",
-      id: "59",
-    },
-  ];
+let tienda = [];
+const getData = async  () => {
+  const data = await fetch("../js/data.json").then((res) => res.json());
+  tienda = data;
+};
 
-let carrito = localStorage.getItem("carrito") ? JSON.parse(localStorage.getItem("carrito")) : {};
+let carrito = localStorage.getItem("carrito")
+  ? JSON.parse(localStorage.getItem("carrito"))
+  : {};
 let buttonClear = document.getElementById("buttonClear");
 
-if(Object.keys(carrito).length === 0){
-  buttonClear.style.display = "none"
+if (Object.keys(carrito).length === 0) {
+  buttonClear.style.display = "none";
 } else {
-  buttonClear.style.display = "block"
+  buttonClear.style.display = "block";
 }
 
 let productosDelCarrito = document.querySelector(".productosDelCarrito");
 
-function agregarElementoAlCarrito(producto){
-    let item = tienda[producto[0]];
+async function agregarElementoAlCarrito(producto) {
+  await getData();
+  let item = tienda[producto[0]];
 
-    let nuevoProducto = document.createElement("div");
+  let nuevoProducto = document.createElement("div");
 
-    let nombreProducto = document.createElement("p");
-    let precioProducto = document.createElement("p");
-    let cantidadProducto = document.createElement("p");
-    let precioTotal = document.createElement("p");
-    let imagenProducto = document.createElement("img");
+  let nombreProducto = document.createElement("p");
+  let precioProducto = document.createElement("p");
+  let cantidadProducto = document.createElement("p");
+  let precioTotal = document.createElement("p");
+  let imagenProducto = document.createElement("img");
 
-    nombreProducto.innerHTML = item.nombre;
-    precioProducto.innerHTML = item.precio;
-    precioTotal.innerHTML = item.precio * producto[1];
-    cantidadProducto.innerHTML = producto[1];
-    imagenProducto.src = "../assets/images" + item.src;
+  nombreProducto.innerHTML = item.nombre;
+  precioProducto.innerHTML = item.precio;
+  precioTotal.innerHTML = item.precio * producto[1];
+  cantidadProducto.innerHTML = producto[1];
+  imagenProducto.src = "../assets/images" + item.src;
 
-    nombreProducto.classList.add("product")
-    precioProducto.classList.add("product")
-    cantidadProducto.classList.add("product")
-    precioTotal.classList.add("product")
-    imagenProducto.classList.add("product")
+  nombreProducto.classList.add("product");
+  precioProducto.classList.add("product");
+  cantidadProducto.classList.add("product");
+  precioTotal.classList.add("product");
+  imagenProducto.classList.add("product");
 
-    nuevoProducto.appendChild(imagenProducto);
-    nuevoProducto.appendChild(nombreProducto);
-    nuevoProducto.appendChild(precioProducto);
-    nuevoProducto.appendChild(cantidadProducto);
-    nuevoProducto.appendChild(precioTotal);
+  nuevoProducto.appendChild(imagenProducto);
+  nuevoProducto.appendChild(nombreProducto);
+  nuevoProducto.appendChild(precioProducto);
+  nuevoProducto.appendChild(cantidadProducto);
+  nuevoProducto.appendChild(precioTotal);
 
-    nuevoProducto.classList.add("contenedorProduct")
+  nuevoProducto.classList.add("contenedorProduct");
 
-    productosDelCarrito.appendChild(nuevoProducto);
-
+  productosDelCarrito.appendChild(nuevoProducto);
 }
 
-for(let producto of Object.entries(carrito)){
-    agregarElementoAlCarrito(producto);
+for (let producto of Object.entries(carrito)) {
+  agregarElementoAlCarrito(producto);
 }
 
-let buttonVaciarCarrito = document.getElementById("buttonClear")
-function vaciarCarrito(){
-  localStorage.clear()
-  window.location.reload()
+function vaciarCarrito() {
+  localStorage.clear();
+  window.location.reload();
 }
-
+let buttonVaciarCarrito = document.getElementById("buttonClear");
 buttonVaciarCarrito.addEventListener("click", vaciarCarrito);
